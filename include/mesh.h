023ac86820dcd5e0c8b3ab8af7f5ea19f383e592
Soham More <msoham2100@gmail.com>
2024-10-31 17:36:06 +0530

    added mesh

--- include/mesh.h
+++ include/mesh.h
@@ -1,11 +1,52 @@
 #pragma once
 
 #include <include/linalg.h>
+#include <include/stack.h>
 
 typedef struct Mesh
 {
+    // stores the grid points
     Vec x;
+    // stores the step sizes
+    // x[i] + dx[i] = x[i + 1]
+    Vec dx;
+    // potential at each point
+    Vec potential;
+
+    // size of mesh
+    size_t len;
+
+    // bounds of the mesh
     double min;
     double max;
+
+    // the value of potential at boundaries
+    double v0;
+    double v1;
+
+    // general info
+    //double maxSpacing;
+    //double minSpacing;
 } Mesh;
 
+// initialize the grid with piecewise uniform sampling
+// assumes ranges are in increasing order
+// Eg: ranges[] = { 0.0, 0.4, 0.5, 1.0 }, sampleCount[] = { 5, 10, 5 }
+// (0.0, 0.4] => 5 samples
+// (0.4, 0.5] => 10 samples
+// (0.5, 1.0) => 5 samples**
+// count = no of elements in sampleCount
+Mesh meshInitPieceUniformA(Vec ranges, size_t sampleCount[], size_t count);
+
+void meshSetDirichletBC(Mesh* mesh, double V0, double V1);
+
+// calculate the first(central) Derivative wrt position for a vector on this mesh
+void meshFirstDerivative(Mesh mesh, Vec var, double boundaries[2], Vec* out);
+
+// calculate the second(central) Derivative wrt position for a vector on this mesh
+void meshSecondDerivative(Mesh mesh, Vec var, double boundaries[2], Vec* out);
+
+// evaluate the poisson equation
+void meshPoissonEvaluate(Mesh mesh, double epsilon, Vec* rho);
+
+void freeMesh(Mesh* mesh);
---
+++ src/mesh.c
@@ -0,0 +1,116 @@
+#include <include/mesh.h>
+
+// initialize the grid with piecewise uniform sampling
+Mesh meshInitPieceUniformA(Vec ranges, size_t sampleCount[], size_t count)
+{
+    Mesh mesh;
+    mesh.len = 0;
+
+    for(size_t i = 0; i < count; i++) mesh.len += sampleCount[i];
+
+    mesh.x = vecInitZerosA(mesh.len);
+    mesh.dx = vecInitZerosA(mesh.len);
+    mesh.potential = vecInitZerosA(mesh.len);
+    mesh.min = VEC_INDEX(ranges, 0);
+    mesh.max = VEC_INDEX(ranges, count);
+
+    size_t point_count = 0;
+    for(size_t i = 0; i < count - 1; i++)
+    {
+        double interval = (VEC_INDEX(ranges, i + 1) - VEC_INDEX(ranges, i));
+        double spacing = interval / (double)(sampleCount[i] + 1);
+
+        for(size_t j = 0; j < sampleCount[i]; j++)
+        {
+            VEC_INDEX(mesh.x, point_count) = VEC_INDEX(ranges, i) + interval * ((double)(j + 1) / (double)(sampleCount[i] + 1));
+            if(point_count > 0) VEC_INDEX(mesh.dx, point_count - 1) = spacing;
+            point_count++;
+        }
+    }
+    double interval = (VEC_INDEX(ranges, count) - VEC_INDEX(ranges, count - 1));
+    double spacing = interval / (double)(sampleCount[count - 1] + 2);
+    double samples = (double)(sampleCount[count - 1] + 2);
+
+    for(size_t i = 0; i < sampleCount[i]; i++)
+    {
+        VEC_INDEX(mesh.x, point_count) = VEC_INDEX(ranges, count - 1) + interval * ((double)(i + 1) / samples);
+        if(point_count > 0) VEC_INDEX(mesh.dx, point_count - 1) = spacing;
+        point_count++;
+    }
+    if(point_count > 0) VEC_INDEX(mesh.dx, point_count - 1) = spacing;
+
+    return mesh;
+}
+
+void meshSetDirichletBC(Mesh* mesh, double V0, double V1)
+{
+    mesh->v0 = V0;
+    mesh->v1 = V1;
+}
+
+// calculate the first Derivative wrt position for a vector on this mesh
+void meshFirstDerivative(Mesh mesh, Vec var, double boundaries[2], Vec* out)
+{
+    double r_b = (VEC_INDEX(mesh.x, 0) - mesh.min) / VEC_INDEX(mesh.dx, 0);
+    double r_f = VEC_INDEX(mesh.dx, 0) / (VEC_INDEX(mesh.x, 0) - mesh.min);
+    double h = VEC_INDEX(mesh.dx, 0) + (VEC_INDEX(mesh.x, 0) - mesh.min);
+
+    VEC_INDEX(*out, 0) = (r_b * VEC_INDEX(var, 1) - r_f * boundaries[0] + (r_b - r_f) * VEC_INDEX(var, 0)) / h;
+
+    for(size_t i = 1; i < mesh.len - 1; i++)
+    {
+        r_b = VEC_INDEX(mesh.dx, i - 1) / VEC_INDEX(mesh.dx, i);
+        r_f = VEC_INDEX(mesh.dx, i) / VEC_INDEX(mesh.dx, i - 1);
+        h = VEC_INDEX(mesh.dx, i) + VEC_INDEX(mesh.dx, i - 1);
+
+        VEC_INDEX(*out, 0) = r_b * VEC_INDEX(var, i + 1) - r_f * VEC_INDEX(var, i - 1) + (r_b - r_f) * VEC_INDEX(var, i);
+    }
+
+    r_b = VEC_INDEX(mesh.dx, mesh.len - 2) / VEC_INDEX(mesh.dx, mesh.len - 1);
+    r_f = VEC_INDEX(mesh.dx, mesh.len - 1) / VEC_INDEX(mesh.dx, mesh.len - 2);
+    h = VEC_INDEX(mesh.dx, mesh.len - 2) + VEC_INDEX(mesh.dx, mesh.len - 1);
+
+    VEC_INDEX(*out, mesh.len - 1) = (r_b * boundaries[1] - r_f * VEC_INDEX(var, mesh.len - 2) + (r_b - r_f) * VEC_INDEX(var, mesh.len - 1)) / h;
+}
+
+// calculate the second Derivative wrt position for a vector on this mesh
+void meshSecondDerivative(Mesh mesh, Vec var, double boundaries[2], Vec* out)
+{
+    double h = VEC_INDEX(mesh.dx, 0) + (VEC_INDEX(mesh.x, 0) - mesh.min);
+    double r_b = 2 * (VEC_INDEX(mesh.x, 0) - mesh.min) / h;
+    double r_f = 2 * VEC_INDEX(mesh.dx, 0) / h;
+    double h_eff = VEC_INDEX(mesh.dx, 0) * (VEC_INDEX(mesh.x, 0) - mesh.min);
+
+    VEC_INDEX(*out, 0) = (r_b * VEC_INDEX(var, 1) + r_f * boundaries[0] - 2.0 * VEC_INDEX(var, 0)) / h_eff;
+
+    for(size_t i = 1; i < mesh.len - 1; i++)
+    {
+        h = VEC_INDEX(mesh.dx, i) + VEC_INDEX(mesh.dx, i - 1);
+        r_b = 2 * VEC_INDEX(mesh.dx, i - 1) / h;
+        r_f = 2 * VEC_INDEX(mesh.dx, i) / h;
+        h_eff = VEC_INDEX(mesh.dx, i) * VEC_INDEX(mesh.dx, i - 1);
+
+        VEC_INDEX(*out, i) = (r_b * VEC_INDEX(var, i + 1) + r_f * VEC_INDEX(var, i - 1) - 2.0 * VEC_INDEX(var, i)) / h_eff;
+    }
+
+    h = VEC_INDEX(mesh.dx, mesh.len - 1) + VEC_INDEX(mesh.dx, mesh.len - 2);
+    r_b = 2 * VEC_INDEX(mesh.dx, mesh.len - 2) / h;
+    r_f = 2 * VEC_INDEX(mesh.dx, mesh.len - 1) / h;
+    h_eff = VEC_INDEX(mesh.dx, mesh.len - 1) * VEC_INDEX(mesh.dx, mesh.len - 2);
+
+    VEC_INDEX(*out, mesh.len - 1) = (r_b * boundaries[1] + r_f * VEC_INDEX(var, mesh.len - 2) - 2.0 * VEC_INDEX(var, mesh.len - 1)) / h_eff;
+}
+
+// evaluate the poisson equation
+void meshPoissonEvaluate(Mesh mesh, double epsilon, Vec* rho)
+{
+    meshSecondDerivative(mesh, mesh.potential, (double[2]){mesh.v0, mesh.v1}, &rho);
+    vecScale(-epsilon, *rho, rho);
+}
+
+void freeMesh(Mesh* mesh)
+{
+    freeVec(&mesh->x);
+    freeVec(&mesh->dx);
+    freeVec(&mesh->potential);
+}
